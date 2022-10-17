59b01fbf4c603d2f7d39cbab91604f4b3677efb7
reece-elder <reece.elder@qa.com>
2022-10-17 11:46:38 +0100

    intro code

---
+++ Code/001-Intro/demo.py
@@ -0,0 +1,39 @@
+# Python is a high level dynamically typed programming language
+# This is a comment, non executable, instructions, explanation etc.
+
+# print is a function that takes in some data (text) and 'prints' it to the console
+# We can run our code by typing 'python <name of file>' in our terminal
+print('Hello World')
+
+# Python works as a scripting language, reading top to bottom of the file you are on
+print("z")
+print("apple")
+print(1)
+
+# dynamically typed data - You don't need to specify the type of data, the interpreter will assume it for you
+
+# Python Data Types
+# int, strings, booleans, var, float, array, char (single character), set, list, tuple, dictionary,
+# Number
+tiers = 2 # ints are whole numbers
+price = 12.99 # floats are decimal (floating point values)
+
+# String (text)
+tiers = "four" # Text
+rating = 'a'   # Char, single character
+
+# Boolean
+decorated = True # Python booleans are Uppercase True or False
+
+# Array (Collection of data types)
+cakeFlavours = ["Mango", "Kiwi", "Vanilla"]
+
+# Variable is a placeholder for data 
+print("============================================")
+print(price)
+price = 10.55
+print(price)
+
+# Exercise - Create variables for the following data types: string, int, float, boolean, array 
+# Use a print function to print the content of those variables 
+# Stretch goal - Experiment with formatting your print statement to make it interesting 'print("price: " + price)'
